f7cde2cf5922e8d492dacb78cb0e82d977d5b3c9
Kyle Ross <odbadger816@gmail.com>
2019-04-06 12:09:58 -0700

    markup for top section

--- template-parts/components/customer-feedback.php
+++ template-parts/components/customer-feedback.php
@@ -2,18 +2,20 @@
   <div class="grid-container">
     <header class="feedback__header">
       <h1>Hear from our customers</h1>
-      <span>Our customers love chamer, we have nothing to hide</span>
+      <span class="sub-header">Our customers love chamer, we have nothing to hide</span>
     </header>
 
     <article class="feedback__quote">
       <div class="grid-x grid-margin-x">
         <div class="cell small-12 large-6">
-          <img class="image" src="<?php echo get_template_directory_uri() . '/assets/img/default-headshot.jpg' ?>" />
+          <div class="feedback__image">
+            <img class="image" src="<?php echo get_template_directory_uri() . '/assets/img/default-headshot.jpg' ?>" />
+          </div>
         </div>
         <div class="cell small-12 large-6">
           <p>Thank guys, keep up the great work! chamer should be nominated for service of the year. you won't regret it</p>
-          <span>Albert Donko</span>
-          <span>CEO, Squaround</span>
+          <span class="name">Albert Donko</span>
+          <span class="title">CEO, Squaround</span>
         </div>
       </div>
     </article>
@@ -22,12 +24,14 @@
     <article class="feedback__quote">
       <div class="grid-x grid-margin-x">
         <div class="cell small-12 large-6 large-order-2">
-          <img class="image" src="<?php echo get_template_directory_uri() . '/assets/img/default-headshot.jpg' ?>" />
+          <div class="feedback__image">
+            <img class="image" src="<?php echo get_template_directory_uri() . '/assets/img/default-headshot.jpg' ?>" />
+          </div>
         </div>
         <div class="cell small-12 large-6">
           <p>Thank guys, keep up the great work! chamer should be nominated for service of the year. you won't regret it</p>
-          <span>Albert Donko</span>
-          <span>CEO, Squaround</span>
+          <span class="name">Albert Donko</span>
+          <span class="title">CEO, Squaround</span>
         </div>
       </div>
     </article>
@@ -36,15 +40,19 @@
     <article class="feedback__quote">
       <div class="grid-x grid-margin-x">
         <div class="cell small-12 large-6">
-          <img class="image" src="<?php echo get_template_directory_uri() . '/assets/img/default-headshot.jpg' ?>" />
+          <div class="feedback__image">
+            <img class="image" src="<?php echo get_template_directory_uri() . '/assets/img/default-headshot.jpg' ?>" />
+          </div>
         </div>
         <div class="cell small-12 large-6">
           <p>Thank guys, keep up the great work! chamer should be nominated for service of the year. you won't regret it</p>
-          <span>Albert Donko</span>
-          <span>CEO, Squaround</span>
+          <span class="name">Albert Donko</span>
+          <span class="title">CEO, Squaround</span>
         </div>
       </div>
     </article>
-    <button class="button">View More</button>
+    <div class="feedback__button">
+      <button class="button">View More</button>
+    </div>
   </div>
 </section>
