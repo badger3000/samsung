90a45ec059533f94160ff861a66bfc24299fb19f
Kyle Ross <odbadger816@gmail.com>
2019-04-07 09:54:54 -0700

    client blog section, in footer

--- footer.php
+++ footer.php
@@ -1,5 +1,45 @@
     <footer>
-      <?php wp_footer();?>
+      <section class="client-blog">
+        <div class="grid-container">
+          <div class="flex-container flex-dir-column large-flex-dir-row">
+            <div class="client-blog__item">
+              <img class="image" src="<?php echo get_template_directory_uri() . '/assets/img/default-headshot.jpg' ?>" />
+              <div class="author">
+                <h5>Elizabeth Martin</h5>
+                <div class="company">SaveSpace Inc.</div>
+                <div class="quote">
+                  “Chamer is the most valuable business resource we have ever purchased. I would be lost without chamer.”
+                </div>
+              </div>
+            </div>
+            <div class="client-blog__item">
+              <img class="image" src="<?php echo get_template_directory_uri() . '/assets/img/default-headshot.jpg' ?>" />
+              <div class="author">
+                <h5>Elizabeth Martin</h5>
+                <div class="company">SaveSpace Inc.</div>
+                <div class="quote">
+                  “Chamer is the most valuable business resource we have ever purchased. I would be lost without chamer.”
+                </div>
+              </div>
+            </div>
+            <div class="client-blog__item">
+              <img class="image" src="<?php echo get_template_directory_uri() . '/assets/img/default-headshot.jpg' ?>" />
+              <div class="author">
+                <h5>Elizabeth Martin</h5>
+                <div class="company">SaveSpace Inc.</div>
+                <div class="quote">
+                  “Chamer is the most valuable business resource we have ever purchased. I would be lost without chamer.”
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="client-blog__button">
+            <button href="/" class="button button--full">view more</button>
+          </div>
+        </div>
+      </section>
+      <?php wp_footer(); ?>
     </footer>
-  </body>
-</html>
+    </body>
+
+    </html>
